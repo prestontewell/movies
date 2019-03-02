ed9bc728445b97c4c5279b5235f027165082f63a
prestontewell <preston@tewellphoto.com>
2019-03-02 15:41:11 -0600

    add index to movies app

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,10 @@
 class Api::MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    render "index.json.jbuilder"
+  end
+
+  
+
 end
---
+++ app/views/api/movies/index.json.jbuilder
@@ -0,0 +1,6 @@
+json.array! @movies.each do |movie|
+  json.id movie.id
+  json.title movie.title
+  json.runtime movie.runtime
+end
+  
